5a738459a72b2cb438629252db45e5dcae6968a2
OrianaManrique <orianamanriqueutlook.com.ar>
2024-12-21 17:20:22 -0300

    Creación de biblioteca de clases, form web, añadidas las referencias.

---
+++ RevisionDelContrato/DAO/AccesoDatos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// cada Dao correspondiente a cada una de las entidades
+/// </summary>
+
+namespace DAO
+{
+    class AccesoDatos
+    {
+        ///conexión
+    }
+}
---
+++ RevisionDelContrato/VISTAS/Formulario.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/// En negocio van las validaciones
+/// trabajar siempre que se pueda con clases
+
+namespace VISTAS
+{
+    public partial class Formulario : System.Web.UI.Page
+    {
+        
+        /// Declaración Negocios
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
